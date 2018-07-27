15be4b23da702ef3fad90b4d85d275017538c2c5
Taras <37515849+ktv1@users.noreply.github.com>
2018-07-28 01:15:01 +0300

    add AccountController

---
+++ resources/views/frontend/account/account.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 28.07.2018
+ * Time: 0:05
+ */
---
+++ vendor/tcg/voyager/src/Http/Controllers/VoyagerResetPasswordController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace TCG\Voyager\Http\Controllers;
+
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+
+class VoyagerResetPasswordController extends Controller
+{
+    use ResetsPasswords;
+
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin';
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+        return view('voyager::auth.reset')->with(
+            ['token' => $token, 'email' => $request->email]
+        );
+    }
+}
